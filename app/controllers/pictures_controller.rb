b5caacb0cfcadae700dbe2ec57794d0302b5a57d
nolalacey <nolalacey@hotmail.com>
2013-11-04 03:47:23 -0500

    Adding feature to create new picture using HTML form

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -43,6 +43,13 @@ class PicturesController < ApplicationController
 		@picture = @pictures[params[:id].to_i]
 	end
 
+	def new
+	end
+
+	def create
+		render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+	end
+
 	# def picture0
 	# 	@picture = {
 	# 		:title => "The old church on the coast of White sea",
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Photogur::Application.routes.draw do
   get 'pictures' => 'pictures#index'
+
+  post 'pictures' => "pictures#create"
+  get 'pictures/new'=> 'pictures#new'
+  
   get 'pictures/:id' => 'pictures#show', as: "picture"
   # get 'pictures/0' => 'pictures#picture0'
   # get 'pictures/1' => 'pictures#picture1'
