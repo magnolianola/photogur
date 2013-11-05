b1b92d71ef409587b299b6f9c181eb6320592830
nolalacey <nolalacey@hotmail.com>
2013-11-05 14:09:51 -0500

    commit

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,87 @@
+class PicturesController < ApplicationController
+	def index
+		@pictures = Picture.all #[
+		# 	{
+		# 		:title => "The old church on the coast of White sea",
+		# 		:artist => "Sergey Ershov",
+		# 		:url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+
+		# 		},
+		# 	{
+		# 		:title => "Sea Power",
+		# 		:artist => "Stephen Scullion",
+		# 		:url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
+		# 	},
+		# 	{
+		# 		:title => "Into the Poppies",
+		# 		:artist => "John Wilhelm",
+		# 		:url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
+		# 	}
+		# ]
+	end
+
+	def show
+		@picture = Picture.find(params[:id])#[
+		# 	{
+		# 		:title => "The old church on the coast of the White sea",
+		# 		:artist => "Sergey Ershov",
+		# 		:url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+
+		# 	},
+		# 	{
+		# 		:title => "Sea Power",
+		# 		:artist => "Stephen Scullion",
+		# 		:url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
+		# 	},
+		# 	{
+		# 		:title => "Into the Poppies",
+		# 		:artist => "John Wilhelm",
+		# 		:url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
+		# 	}
+				
+		# ]
+		# @picture = @pictures[params[:id].to_i]
+	end
+
+	def new
+		@picture = Picture.new
+	end
+
+	def create
+		#make a new picture with what picture_params returns (which is a method we're calling)
+		@picture = Picture.new(picture_params)
+		if @picture.save
+			#if the save for the picture was successful, go to index.html.erb
+			redirect_to pictures_url
+		else
+			#otherwise render the view associated with the action :new (i.e. new.html.erb)
+			render :new
+		end
+		# render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+	end
+
+	def edit
+		@picture = Picture.find(params[:id])
+	end
+
+	def update
+		@picture = Picture.find(params[:id])
+
+		if @picture.update_attributes(picture_params)
+			redirect_to "/pictures/#{@picture.id}"
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@picture = Picture.find(params[:id])
+		@pixture.destroy
+		redirect_to pictures_url
+	end
+
+	private
+	def picture_params
+		params.require(:picture).permit(:artist, :title, :url)
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Photogur::Application.config.secret_key_base = '1adf393408d665daae16a8e3c8d84d90a9ac1385c9fe3e325aa65ebdbf4fb375b57dcf2a498d57887575c8a3879aee9bdc4efab041068fe255594a7459a3ce19'
